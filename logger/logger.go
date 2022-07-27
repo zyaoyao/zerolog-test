a4e43f6053c4a2e5b8f8c8e17204b07ed4b48ba9
zxzzg <93zxzzgezyy>
2022-07-27 17:33:31 +0800

    test commit

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,39 @@
 package logger
+
+import (
+	"fmt"
+	"github.com/rs/zerolog"
+	"os"
+	"time"
+)
+
+var (
+	log zerolog.Logger
+)
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	log = zerolog.New(output).With().Timestamp().Logger()
+	fmt.Println("test")
+}
+
+func Infof(format string, v ...interface{}) {
+	log.Info().Msgf(format, v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	log.Debug().Msgf(format, v...)
+}
+
+func Warnf(format string, v ...interface{}) {
+	log.Warn().Msgf(format, v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	log.Error().Msgf(format, v...)
+}
+
+func Panicf(format string, v ...interface{}) {
+	log.Panic().Msgf(format, v...)
+}
